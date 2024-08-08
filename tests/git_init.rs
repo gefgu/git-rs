bb4bcc5bea75ec749d258bdb53b2c7b833546194
Gustavo Santos <gefgu@hotmail.com>
2024-08-08 19:09:11 -0300

    test: already has git folder

--- tests/git_init.rs
+++ tests/git_init.rs
@@ -1,18 +1,18 @@
-use std::{fs, path::Path, thread::sleep, time::Duration};
+use std::{fs, path::Path};
 
 use assert_cmd::Command;
+use predicates::prelude::*;
 
 #[test]
 fn emtpy_folder() -> Result<(), Box<dyn std::error::Error>> {
     let folder = assert_fs::TempDir::new().unwrap();
+    let dir_path = folder.path();
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
 
-    cmd.current_dir(folder.path());
+    cmd.current_dir(dir_path);
 
     cmd.arg("init");
     cmd.assert().success();
-
-    sleep(Duration::from_secs(15));
 
     assert_eq!(Path::new(".git").exists(), true);
     assert_eq!(Path::new(".git/objects").exists(), true);
@@ -21,6 +21,27 @@ fn emtpy_folder() -> Result<(), Box<dyn std::error::Error>> {
 
     let head_content = fs::read_to_string(".git/HEAD")?;
     assert_eq!(head_content, "ref: refs/heads/main\n");
+
+    folder.close()?;
+    Ok(())
+}
+
+#[test]
+fn already_has_git_folder() -> Result<(), Box<dyn std::error::Error>> {
+    let folder = assert_fs::TempDir::new().unwrap();
+    Command::new("git")
+        .current_dir(folder.path())
+        .arg("init")
+        .assert()
+        .success();
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
+
+    cmd.current_dir(folder.path());
+
+    cmd.arg("init");
+    cmd.assert().success().stdout(predicate::str::contains(
+        "Reinitialized existing Git repository in",
+    ));
 
     folder.close()?;
     Ok(())
